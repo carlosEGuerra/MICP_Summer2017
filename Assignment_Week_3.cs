b962ad8a7f3347a367bcbca9a9bda6435e176b74
Carlos E Guerra Chan <carlosguerraworkmail@gmail.com>
2017-08-06 16:54:27 -0600

    Assignment 3 Files
    O(N^3) complexity, and need to further test the program

---
+++ Assignment_Week_3.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace MICP_Assignments
+{
+    public class Assignment_Week_3
+    {
+        public static int[][] uniqueTripletCombinations(int[] userInput)
+        {
+            //checks to ensure that there IS input into the array
+            if (userInput == null || userInput.Length <= 0)
+                return null;
+
+            //if there isn't enough input
+            if (userInput.Length < 3)
+                throw new ArgumentException();
+
+            //used to contain the pending results
+            List<int[]> results = new List<int[]>();
+
+            //organizes the user input
+            Array.Sort(userInput);
+
+            for(int firstIndex = 0; firstIndex < userInput.Length - 3; firstIndex++)
+            {
+               for(int secondIndex = firstIndex + 1; secondIndex < userInput.Length - 2; secondIndex++)
+                {
+                    for(int thirdIndex = secondIndex + 1; thirdIndex < userInput.Length - 1; thirdIndex++)
+                    {
+                        if((userInput[firstIndex] + userInput[secondIndex] + userInput[thirdIndex]) == 0)
+                        {
+                            int[] temp = new int[3];
+                            temp[0] = userInput[firstIndex];
+                            temp[1] = userInput[secondIndex];
+                            temp[2] = userInput[thirdIndex];
+                            results.Add(temp);
+                        }
+                    }
+                }
+            }
+
+            return results.ToArray();
+        }
+    }
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MICP_Assignments;
+using System.Collections.Generic;
+
+namespace UnitTestsForAssignment3
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private int[] testArray = new int[10];
+
+        [TestInitialize]
+        public void arrayInitializationForTesting()
+        {
+            Random rand = new Random();
+            for(int i = 0; i < testArray.Length; i++)
+            {
+                testArray[i] = rand.Next() % 10;
+            }
+        }
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Console.WriteLine(set);
+            Console.ReadLine();
+        }
+    }
+}
