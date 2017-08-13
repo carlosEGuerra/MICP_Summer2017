b2a90b813d3b0acd5b51f2da51b7f6af565b88ac
Carlos E Guerra Chan <carlosguerraworkmail@gmail.com>
2017-08-12 22:27:01 -0600

    Assignment Week 4
    O(n^2) complexity, can be optimized using recursive methods as described by TEBOWIT(uploading on Sunday Afternoon)

---
+++ Unit Tests for Assignment 4/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Unit Tests for Assignment 4")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Unit Tests for Assignment 4")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("685807eb-2b23-4ce2-8327-415a1e72c542")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
